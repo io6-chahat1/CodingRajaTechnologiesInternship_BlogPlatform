32d2cf813c31f3eba3af6048a2e56dd1f66f5d73
prerna82 <sachinmodgill548@gmail.com>
2024-06-05 00:23:21 +0530

    added use

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='py-10 text-center'>
         Copyright © 2024 All rights reserved.
         <br />
-        This website is made with ♡ by Chahat.
+        This website is made with ♡ by Chahat Rajput.
     </div>
   )
 }
